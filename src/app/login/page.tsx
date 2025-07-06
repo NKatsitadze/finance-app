03ac1e8c1e3ebecccdf4ec37ea0dc7747a16768a
Nika <nikakatsitadze98@gmail.com>
2025-07-06 18:11:41 +0400

    update responsiveness

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,6 +4,9 @@ import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { signInWithPopup } from 'firebase/auth'
 import { auth, provider } from '@/lib/firebase'
+import loginImage from '/assets/images/login.png'
+import IconLogoLarge from '@/components/IconComponents/IconLogoLarge'
+import Button from '@/components/DesignSystem/Button'
 
 export default function LoginPage() {
   const router = useRouter()
@@ -19,18 +22,40 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
-      <div className="bg-white p-6 rounded shadow-md w-full max-w-sm">
-        <h2 className="text-2xl font-semibold mb-4 text-center">Login</h2>
+    <div className="flex h-screen overflow-hidden background-beige-100 p-4 gap-10" style={{border:'1px solid red'}}>
 
-        {error && <p className="text-red-600 text-sm mb-3">{error}</p>}
+      <div className="hidden overflow-hidden items-center flex md:block md:w-2/5 rounded-xl relative">
+        <img
+          src={loginImage.src}
+          alt="login image"
+          className="w-full h-full  object-cover object-top rounded-xl"
+        />
 
-        <button
-          onClick={handleGoogleLogin}
-          className="w-full bg-red-600 text-white py-2 rounded hover:bg-red-700"
-        >
-          Sign in with Google
-        </button>
+        <div className='absolute top-0 left-0 p-10 text-white h-full w-full flex flex-col justify-between'>
+          <IconLogoLarge/>
+          <section className="flex flex-col gap-6">
+            <h1 className="text-preset-1 font-bold"
+              style={{ textShadow: '0px 2px 4px rgba(0, 0, 0, 0.5)' }}
+            >
+              Keep track of your money and save for your future
+            </h1>
+            <p className="text-preset-4"
+              style={{ textShadow: '0px 2px 4px rgba(0, 0, 0, 0.5)' }}
+            >
+              Personal finance app puts you in control of your spending. Track transactions, set budgets, and add to savings pots easily.
+            </p>
+          </section>
+        </div>
+      </div>
+
+      <div className="w-full md:w-3/5 flex items-center justify-center ">
+        <div className="bg-white p-8 flex flex-col gap-8 rounded-xl shadow-md w-full max-w-[560px]">
+          <h2 className="text-preset-1 bold text-grey-900 text-center">Login</h2>
+
+          {error && <p className="text-red-600 text-sm mb-3">{error}</p>}
+
+          <Button label='Sign in with Google' onButtonClick={handleGoogleLogin}/>
+        </div>
       </div>
     </div>
   )
